3ecce6344f7c6158eaeb9683bb3c7e11c8c86673
maj39623 <maj39623@gmail.com>
2022-07-06 11:29:06 -0400

    90%

---
+++ src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+
+    <DataBoxes :stats="stats" />
+
+    <CountrySelect :countries="countries" />
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="" />
+  </main>
+</template>
+
+<script>
+// @ is an alias to /src
+import DataTitle from "@/components/DataTitle.vue";
+import DataBoxes from "@/components/DataBoxes.vue";
+import CountrySelect from "@/components/CountrySelect.vue";
+
+export default {
+  name: "Home",
+  components: {
+    DataTitle,
+    DataBoxes,
+    CountrySelect,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: " ",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.dataDate;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
+</script>
